8d7750ee520d056e33cc3f237680587bb2644e6b
JanosSandor2002 <sandorj2002@gmail.com>
2026-01-05 14:02:33 +0100

    update

--- 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react';
+import data from './data';
+
 const App = () => {
+  const [people, setPeople] = useState(data);
+  useEffect(() => {
+    console.log(data);
+  }, []);
+
   return <h2>Birthday Reminder - Starter</h2>;
 };
 export default App;
