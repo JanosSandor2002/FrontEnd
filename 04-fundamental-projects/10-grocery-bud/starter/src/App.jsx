8838ab95e2e6f8506b7ccf6f79818b390dfcb18c
JanosSandor2002 <sandorj2002@gmail.com>
2026-01-14 15:30:13 +0100

    10 readied

--- 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
+++ 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
@@ -1,5 +1,135 @@
-const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { ToastContainer, toast } from 'react-toastify';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = [];
+  }
+  return list;
 };
 
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
+const defaultList = JSON.parse(localStorage.getItem('list') || '[]');
+const App = () => {
+  const [items, setItems] = useState(defaultList);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success('item added to the list');
+  };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success('item deleted');
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+  return (
+    <section className='section-center'>
+      <ToastContainer position='top-center' />
+      <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
+    </section>
+  );
+};
 export default App;
+
+const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItemName) {
+      toast.error('please provide value');
+      return;
+    }
+    addItem(newItemName);
+    setNewItemName('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>grocery bud</h4>
+      <div className='form-control'>
+        <input
+          type='text '
+          className='form-input'
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type='submit' className='btn'>
+          add item
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Items = ({ items, removeItem, editItem }) => {
+  return (
+    <div className='items'>
+      {items.map((item) => {
+        return (
+          <SingleItem
+            key={item.id}
+            item={item}
+            removeItem={removeItem}
+            editItem={editItem}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const SingleItem = ({ item, removeItem, editItem }) => {
+  return (
+    <div className='single-item'>
+      <input
+        type='checkbox'
+        checked={item.completed}
+        onChange={() => editItem(item.id)}
+      />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: item.completed && 'line-through',
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        className='btn remove-btn'
+        type='button'
+        onClick={() => removeItem(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
