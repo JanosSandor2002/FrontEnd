ba43a9907c14c6d3d05abf765467112c98217b03
JanosSandor2002 <sandorj2002@gmail.com>
2026-01-17 13:48:02 +0100

    14 cart readied

---
+++ 04-fundamental-projects/14-cart/starter/src/reducer.js
@@ -0,0 +1,47 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  DISPLAY_ITEMS,
+  LOADING,
+} from './actions';
+
+const reducer = (state, action) => {
+  let newCart, itemId, item, newItem;
+  switch (action.type) {
+    case CLEAR_CART:
+      return { ...state, cart: new Map() };
+    case REMOVE:
+      newCart = new Map(state.cart);
+      newCart.delete(action.payload.id);
+      return { ...state, cart: newCart };
+    case INCREASE:
+      newCart = new Map(state.cart);
+      itemId = action.payload.id;
+      item = newCart.get(itemId);
+      newItem = { ...item, amount: item.amount + 1 };
+      newCart.set(itemId, newItem);
+      return { ...state, cart: newCart };
+    case DECREASE:
+      newCart = new Map(state.cart);
+      itemId = action.payload.id;
+      item = newCart.get(itemId);
+
+      if (item.amount === 1) {
+        newCart.delete(itemId);
+        return { ...state, cart: newCart };
+      }
+      newItem = { ...item, amount: item.amount - 1 };
+      newCart.set(itemId, newItem);
+      return { ...state, cart: newCart };
+    case LOADING:
+      return { ...state, loading: true };
+    case DISPLAY_ITEMS:
+      newCart = new Map(action.payload.cart.map((item) => [item.id, item]));
+      return { ...state, loading: false, cart: newCart };
+    default:
+      throw new Error('no matching for this type of reducer type');
+  }
+};
+export default reducer;
