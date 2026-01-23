c762d8c238b3615aa1ff519a3b9b5802c9a2d4e8
JanosSandor2002 <sandorj2002@gmail.com>
2026-01-24 00:37:17 +0100

    axios readied

--- 05-axios-tutorial/src/examples/6-interceptors.js
+++ 05-axios-tutorial/src/examples/6-interceptors.js
@@ -1,10 +1,16 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
 
 const url = 'https://www.course-api.com/react-store-products';
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = await authFetch('/react-store-products');
+      console.log(resp);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
